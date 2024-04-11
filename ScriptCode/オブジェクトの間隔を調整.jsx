984f77452f846687381dc9a80b7a34c1c85237b8
古橋 宏 <bryful@gmail.com>
2024-04-11 19:13:28 +0900

    update

--- ScriptCode/オブジェクトの間隔を調整.jsx
+++ ScriptCode/オブジェクトの間隔を調整.jsx
@@ -1,17 +1,18 @@
 (function (me){
 	var pref = {};
-	pref.unitMode = 0;
+	pref.unitMode = 1;
 	pref.moveV = 10;
-	pref.dpi = 300;
+	pref.dpi = 72;
 	pref.anchorMode= 1;
+	pref.mode =0;
 
-	var scriptName ="動的配置-垂直";
+	var scriptName ="オブジェクトの間隔を調整";
 
- // 環境設定ファイルのFileオブジェクトを返す
-    // 無かったらフォルダの作成も行う
-    // "C:\Users\<ユーザー名>\AppData\Roaming\bry-ful\aep置き換え\aep置き換え.pref"
-    var getPrefFile = function()
-    {
+	// 環境設定ファイルのFileオブジェクトを返す
+	// 無かったらフォルダの作成も行う
+	// "C:\Users\<ユーザー名>\AppData\Roaming\bry-ful\aep置き換え\aep置き換え.pref"
+	var getPrefFile = function()
+	{
         var ret = "noret";
         var pff = "none";
         var pff2 = "none2";
@@ -48,9 +49,9 @@
         if (ret == "noret") ret = null;
         return ret;
 
-    }
+	}
 	 var loadPref = function()
-    {
+	{
         var prefFile = getPrefFile();
         if((prefFile!=null)&&(prefFile instanceof File)&&(prefFile.exists))
         {
@@ -60,6 +61,8 @@
                 try{
                     s = prefFile.read();
                     var obj = eval(s);
+					v = obj.mode;
+					if((v!=undefined)&&(typeof(v)=="number")){ pref.mode = v; v=null;}
 					v = obj.moveV;
 					if((v!=undefined)&&(typeof(v)=="number")){ pref.moveV = v; v=null;}
 					v = obj.unitMode;
@@ -67,13 +70,6 @@
 						if (v<0){v=0;}else if (v>2){v=2;}
 						pref.unitMode = v;
 						v=null;
-					}
-					v = obj.dpi;
-					if((v!=undefined)&&(typeof(v)=="number"))
-					{
-						if (v<72) {v=72;}
-						pref.dpi = v;
-						 v=null;
 					}
 					v = obj.anchorMode;
 					if((v!=undefined)&&(typeof(v)=="number"))
@@ -89,11 +85,11 @@
                 }
     		}
         }
-    }
-    loadPref();
-    //-------------------------------------------------------------------------
-    var savePref = function()
-    {
+	}
+	loadPref();
+	//-------------------------------------------------------------------------
+	var savePref = function()
+	{
         var prefFile = getPrefFile();
         if ( (prefFile!=null)&&(prefFile instanceof File))
         {
@@ -110,7 +106,7 @@
         }else{
             alert("savePref : no prefFile!\r\n");
         }
-    }
+	}
 	//-------------------------------------------------------------------------
 	Number.prototype.pointToMM = function()
 	{
@@ -128,24 +124,8 @@
 			alert(e.toString());
 		}
 	}
-	Number.prototype.mmToPx = function(dpi)
-	{
-		try{
-			return this*dpi/25.4;
-		}catch(e){
-			alert(e.toString());
-		}
-	}
-	Number.prototype.pxToMM = function(dpi)
-	{
-		try{
-			return this*25.4/dpi;
-		}catch(e){
-			alert(e.toString());
-		}
-	}
 	// ****************************************************************************************************************
-	var sortY= function(objs)
+	var sortObj= function(objs)
 	{
 		function sortYsub(a,b)
 		{
@@ -162,44 +142,85 @@
 				return -1;
 			}
 		}
+		function sortXsub(a,b)
+		{
+			var ax = a.left + a.width/2;
+			var bx = b.left + b.width/2;
+			if (ax == bx)
+			{
+				return 0;
+			}
+			else if (ax>bx)
+			{
+				return 1;
+			}else{
+				return -1;
+			}
+		}
 		var ret = objs;
-		ret.sort(sortYsub)
+		if (pref.mode==0)
+		{
+			ret = ret.sort(sortXsub)
+		}else{
+			ret = ret.sort(sortYsub)
+		}
 		return ret;
 	}
 	// ****************************************************************************************************************
 	var getObjPos = function(obj)
 	{
 		var ret = 0;
-		ret =  obj.top - obj.height/2;
+		if(pref.mode==0)
+		{
+			ret =  obj.left + obj.width/2;
+		}else{
+			ret =  obj.top - obj.height/2;
+		}
 		return ret;
 	}
 	var setObjPos = function(obj,v)
 	{
-		obj.top =  v + obj.height/2;
+		if(pref.mode==0)
+		{
+			obj.left =  v - obj.width/2;
+		}else{
+			obj.top  =  v + obj.height/2;
+		}
 	}
 	// ****************************************************************************************************************
 	var moveObject = function(mv,mode)
 	{
 		var selObj = app.activeDocument.selection;
 		if(selObj.length<=1) return;
-		selObj = sortY(selObj);
+		selObj = sortObj(selObj);
 
-		var s= "";
-		for(var i=0; i<selObj.length;i++)
+		var s = "";
+		for (var i=0; i<selObj.length;i++)
 		{
-			s += selObj[i].top + "\r\n";
+			s += "l:" + selObj[i].left + "t:" + selObj[i].top +"r\n";
 		}
-		var minY= -selObj[0].top + selObj[0].height/2;
-		var maxY= -selObj[selObj.length-1].top + selObj[selObj.length-1].height/2;
+		//alert(s);
+		var minV=0;
+		var maxV=0;
+		if (pref.mode==0)
+		{
+			minV= selObj[0].left + selObj[0].width/2;
+			maxV= selObj[selObj.length-1].left + selObj[selObj.length-1].width/2;
 
-		var srcLen = maxY - minY;
+		}else{
+			minV= -selObj[0].top + selObj[0].height/2;
+			maxV= -selObj[selObj.length-1].top + selObj[selObj.length-1].height/2;
+
+		}
+
+		var srcLen = maxV - minV;
 		var cnt = selObj.length-1;
 
 		switch(pref.anchorMode)
 		{
 			case 0:
 				var len = 0;
-				var start = minY;
+				var start = minV;
 				switch(mode)
 				{
 					case 0:
@@ -212,9 +233,11 @@
 						len = mv;
 						break;
 				}
+				var op=1;
+				if (pref.unitMode==1) op=-1;
 				for(var i=0; i<selObj.length;i++)
 				{
-					setObjPos(selObj[i],-start);
+					setObjPos(selObj[i],start*op);
 					start += len;
 				}
 				break;
@@ -232,17 +255,19 @@
 						newLen = mv*cnt;
 						break;
 				}
-				start = (minY+maxY)/2 - newLen/2;
+				start = (minV+maxV)/2 - newLen/2;
 				len = newLen/cnt;
 				for(var i=0; i<selObj.length;i++)
 				{
-					setObjPos(selObj[i],-start);
+				var op=1;
+				if (pref.unitMode==1) op=-1;
+					setObjPos(selObj[i],start*op);
 					start += len;
 				}
 				break;
 			case 2:
 				var len = 0;
-				var start = maxY;
+				var start = maxV;
 				switch(mode)
 				{
 					case 0:
@@ -255,106 +280,73 @@
 						len = mv;
 						break;
 				}
+				var op=1;
+				if (pref.unitMode==1) op=-1;
 				for(var i=selObj.length-1; i>=0;i--)
 				{
-					setObjPos(selObj[i],-start);
+					setObjPos(selObj[i],start*op);
 					start -= len;
 				}
 				break;
 		}
 		app.redraw();
 	}
-	// ****************************************************************************************************************
-	var createDialog = function()
+	// ******************************************************************
+	var getED = function(ed)
 	{
-		if (app.activeDocument==null) return;
-		if (app.activeDocument.selection.length<=1) return;
-		function getMoveV()
+		var ret = null;
+		try{
+			var o = eval(ed.text);
+			if (typeof(o)=="number")
+			{
+				ret = o;
+			}
+		}catch(e){
+		}
+		return ret;
+	}
+	// ****************************************************************************************************************
+	var createUI = function()
+	{
+		function getValue()
 		{
 			var ret = null;
 			var mv = pref.moveV;
-			try{
-				mv = eval(edValue.text);
-
-				if (typeof(mv)=="number")
-				{
-					ret = mv;
-					pref.moveV = mv;
-				}
-				if (mv==0)
-				{
-					ret = null;
-					return ret;
-				}
-			}catch(e){
-				alert("パラメータ異常\r\n"+e.toString());
-				ret =null;
-			}
-			switch(pref.unitMode)
+			ret = getED(edValue);
+			if ((ret == null)||(ret <=0))
 			{
-				case 0:
-					ret = ret.mmToPoint();
-					break;
-				case 1:
-					break;
-				case 2:
-					try{
-						dpi = eval(edDpi.text);
-						if (typeof(dpi)!="number")
-						{
-							ret = null;
-							return;
-						}
-						if (dpi<72)
-						{
-							dpi = 72;
-							edDpi.text = dpi+"";
-						}
-						pref.dpi = dpi;
-						ret = ret.pxToMM(pref.dpi).mmToPoint();
-					}catch(e){
-						alert("dpi異常\r\n"+e.toString());
-						ret =null;
-					}
-					break;
-
-
+				ret = null;
+				alert("value reeor!");
+				return ret;
 			}
+			if (pref.unitMode==0) ret.mmToPoint();
 			return ret;
 		}
-
-		var winObj = new Window("dialog",scriptName ,[0,0,280,170]);
+		var w = 300;
+		var winObj = new Window("dialog",scriptName ,[0,0,w,180]);
 		x = 5;
 		y = 5;
+		var gunit = winObj.add("group",[x,y,x+270,y+30]);
+		var cbm =[];
+		var x0=0;
+		var y0=0;
+		cbm.push(gunit.add("radiobutton",[x0,y0,x0+70,y0+25],"水平方向"));
+		x0 +=70;
+		cbm.push(gunit.add("radiobutton",[x0,y0,x0+70,y0+25],"垂直方向"));
+		x0 +=70;
+		y += 30;
 		var edValue = winObj.add("edittext",[x,y,x+270,y+25],pref.moveV+"");
 		y += 30;
 		var gunit = winObj.add("group",[x,y,x+270,y+30]);
 		var cbs =[];
-		var x0=0;
-		var y0=0;
+		x0=0;
+		y0=0;
 		cbs.push(gunit.add("radiobutton",[x0,y0,x0+55,y0+25],"mm"));
-		x0 +=60;
-		cbs.push(gunit.add("radiobutton",[x0,y0,x0+55,y0+25],"Point"));
 		x0 +=60;
 		cbs.push(gunit.add("radiobutton",[x0,y0,x0+55,y0+25],"Pixel"));
 		x0 +=60;
-		var stDpi = gunit.add("statictext",[x0,y0,x0+30,y0+25],"dpi");
-		x0 += 30;
-		var edDpi = gunit.add("edittext",[x0,y0,x0+50,y0+25],pref.dpi);
 
-		cbs[pref.unitMode].value = true;
-		edDpi.enabled = (pref.unitMode ==2);
 
-		for(var i=0;i<cbs.length;i++)
-		{
-			cbs[i].cIndex=i;
-			cbs[i].onClick = function()
-			{
-				pref.unitMode = this.cIndex;
-				edDpi.enabled = (pref.unitMode ==2);
-				savePref();
-			}
-		}
 		x = 5;
 		y += 25;
 		var stAnchor = winObj.add("statictext",[x,y,x+270,y+30],"配置基準");
@@ -363,12 +355,56 @@
 		x0=0;
 		y0=0;
 		var cba =[];
-		cba.push(ganchor.add("radiobutton",[x0,y0,x0+80,y0+25],"上合わせ"));
+		cba.push(ganchor.add("radiobutton",[x0,y0,x0+90,y0+25],"上合わせ"));
 		x0 +=90;
-		cba.push(ganchor.add("radiobutton",[x0,y0,x0+80,y0+25],"中央合わせ"));
+		cba.push(ganchor.add("radiobutton",[x0,y0,x0+90,y0+25],"中央合わせ"));
 		x0 +=90;
-		cba.push(ganchor.add("radiobutton",[x0,y0,x0+80,y0+25],"下合わせ"));
+		cba.push(ganchor.add("radiobutton",[x0,y0,x0+90,y0+25],"下合わせ"));
 		x0 +=90;
+		y+=25;
+		var btnAdd = winObj.add("button",[x,y,x+80,y+30],"間隔に加算");
+		x+=85;
+		var btnInc = winObj.add("button",[x,y,x+80,y+30],"間隔に減算");
+		x+=95;
+		var btnEql = winObj.add("button",[x,y,x+80,y+30],"間隔に設定");
+
+		function setMode()
+		{
+			switch(pref.mode)
+			{
+				case 0:
+					cba[0].text = "左合わせ"
+					cba[1].text = "中央合わせ"
+					cba[2].text = "右合わせ"
+					break;
+				case 1:
+					cba[0].text = "上合わせ"
+					cba[1].text = "中央合わせ"
+					cba[2].text = "下合わせ"
+					break;
+			}
+		}
+		for(var i=0;i<cbm.length;i++)
+		{
+			cbm[i].cIndex=i;
+			cbm[i].onClick = function()
+			{
+				pref.mode = this.cIndex;
+				setMode();
+				savePref();
+			}
+		}
+		cbm[pref.mode].value = true;
+		setMode();
+		for(var i=0;i<cbs.length;i++)
+		{
+			cbs[i].cIndex=i;
+			cbs[i].onClick = function()
+			{
+				pref.unitMode = this.cIndex;
+				savePref();
+			}
+		}
 		cba[pref.anchorMode].value = true;
 		for(var i=0;i<cba.length;i++)
 		{
@@ -379,27 +415,24 @@
 				savePref();
 			}
 		}
-		y+=25;
-		var btnAdd = winObj.add("button",[x,y,x+80,y+30],"間隔に加算");
-		x+=85;
-		var btnInc = winObj.add("button",[x,y,x+80,y+30],"間隔に減算");
-		x+=95;
-		var btnEql = winObj.add("button",[x,y,x+80,y+30],"間隔に設定");
+		cbs[pref.unitMode].value = true;
+
+
 		btnAdd.onClick = function()
 		{
-			var w = getMoveV();
+			var w = getValue();
 			if(w==null) return;
 			moveObject(w,0);
 		}
 		btnInc.onClick = function()
 		{
-			var w = getMoveV();
+			var w = getValue();
 			if(w==null) return;
 			moveObject(w,1);
 		}
 		btnEql.onClick = function()
 		{
-			var w = getMoveV();
+			var w = getValue();
 			if(w==null) return;
 			moveObject(w,2);
 		}
@@ -409,6 +442,16 @@
 		winObj.show();
 
 	}
-	createDialog();
-	savePref();
+	if (app.activeDocument!=null)
+	{
+		if (app.activeDocument.selection.length>1)
+		{
+			createUI();
+		}else{
+			alert("オブジェクトを２個以上選択してください");
+		}
+
+	}else{
+		alert("no document");
+	}
 })(this);
